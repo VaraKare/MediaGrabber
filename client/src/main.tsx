e5ba25156bab3bbf94a8b6abbafed90f3f1a5f07
Vara k <karewarvaraprasad@gmail.com>
2025-09-19 10:55:07 +0530

    build issue fix

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./app";
 import "./index.css";
 import { QueryClientProvider } from "@tanstack/react-query";
-import queryClient from "@/lib/queryClient";
+import { queryClient } from "@/lib/queryClient";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
